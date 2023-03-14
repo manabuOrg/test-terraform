4425f619492240e0ca2d509c94ece4ec7fb34b6e
Manabu McCloskey <manabu.mccloskey@gmail.com>
2023-03-14 16:49:02 -0600

    Create variables.tf

---
+++ demo-resources/variables.tf
@@ -0,0 +1,17 @@
+variable "name" {
+  description = "Name of the VPC and EKS Cluster"
+  default     = "my-backstage-bucket"
+  type        = string
+}
+
+variable "region" {
+  description = "region"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "duration" {
+  description = "duration to wait in seconds"
+  type        = string
+  default     = "60s"
+}
